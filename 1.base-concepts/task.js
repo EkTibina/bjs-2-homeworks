e70813ae55b454a2449628c64518881666b29f81
Katrusy <e.tibina@inbox.ru>
2026-03-23 22:05:33 +0300

    Задачи 1, 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,47 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  const d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return [];
+  }
+
+  if (d === 0) {
+    return [-b / (2 * a)];
+  }
+
+  const x1 = (-b + Math.sqrt(d)) / (2 * a);
+  const x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+  return [x1, x2];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (
+    typeof percent !== "number" ||
+    typeof contribution !== "number" ||
+    typeof amount !== "number" ||
+    typeof countMonths !== "number"
+  ) {
+    return NaN;
+  }
+
+  const percentDecimal = percent / 100;
+  const monthlyPercent = percentDecimal / 12;
+
+  const creditBody = amount - contribution;
+
+  if (creditBody <= 0) {
+    return 0;
+  }
+
+  const monthlyPayment =
+    creditBody *
+    (monthlyPercent +
+      monthlyPercent / ((1 + monthlyPercent) ** countMonths - 1));
+
+  const totalAmount = monthlyPayment * countMonths + contribution;
+
+  return Number(totalAmount.toFixed(2));
 }
